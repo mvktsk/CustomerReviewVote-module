fdd20085d5bf3009cb871af9e78fd9cdf311b985
Mvk_tsk <maks.kopnov@gmail.com>
2019-01-18 17:42:30 +0700

    123

--- newManagedModule.Core/Model/CustomerReview.cs
+++ newManagedModule.Core/Model/CustomerReview.cs
@@ -7,9 +7,13 @@ namespace newManagedModule.Core.Model
     {
         public string AuthorNickname { get; set; }
         public string Content { get; set; }
-        public bool   IsActive { get; set; }
+        public bool IsActive { get; set; }
         public string ProductID { get; set; }
 
+        public int? TotalVoteIdx { get; set; }
+
+        public virtual ObservableCollection<CustomerReviewVote> CustomerReviewVotes { get; set; }
         
+
     }
 }
