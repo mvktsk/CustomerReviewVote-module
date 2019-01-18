d217f64abadde7f5555f1c15bbfa2a9202b9e110
Mvk_tsk <maks.kopnov@gmail.com>
2019-01-18 17:42:49 +0700

    no message

--- newManagedModule.Core/Model/CustomerReviewVote.cs
+++ newManagedModule.Core/Model/CustomerReviewVote.cs
@@ -4,12 +4,12 @@ namespace newManagedModule.Core.Model
 {
     public class CustomerReviewVote : AuditableEntity
     {
-        public string VoteAuthorNickname { get; set; }
+        public string AuthorId { get; set; }
         public int VoteIdx { get; set; }
         public bool IsActive { get; set; }
 
-        public string CustomerReviewId { get; set; }
-        public CustomerReview CustomerReview { get; set; }
+//       public string CustomerReviewId { get; set; }
+//        public CustomerReview CustomerReview { get; set; }
     }
    
 }
