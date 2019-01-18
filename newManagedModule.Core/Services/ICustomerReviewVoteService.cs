923bc40245cf0936c0088fd6bb69ff8d1cb805c2
Mvk_tsk <maks.kopnov@gmail.com>
2019-01-18 17:43:07 +0700

    no message

---
+++ newManagedModule.Core/Services/ICustomerReviewVoteService.cs
@@ -0,0 +1,11 @@
+﻿using newManagedModule.Core.Model;
+
+namespace newManagedModule.Core.Services
+{
+    public interface ICustomerReviewVoteService
+    {
+        CustomerReviewVote[] GetByIds(string[] ids);
+
+        void SaveCustomerReviewVotes(CustomerReviewVote[] items);
+    }
+}
