23b393bf9056ed4055b6745c60efd840d9779415
Maksim Kopnov <maks_kopnov@rambler.ru>
2019-02-04 11:47:39 +0700

    Fix

--- newManagedModule.Web/Module.cs
+++ newManagedModule.Web/Module.cs
@@ -1,10 +1,10 @@
-﻿using System.Linq;
-using CustomerReviewVotes.Core.Services;
+﻿using CustomerReviewVotes.Core.Services;
 using CustomerReviewVotes.Data.Repositories;
 using CustomerReviewVotes.Data.Services;
 using Microsoft.Practices.Unity;
-using VirtoCommerce.Platform.Core.Modularity;
+using System.Linq;
 using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.Platform.Data.Infrastructure;
 using VirtoCommerce.Platform.Data.Infrastructure.Interceptors;
@@ -14,7 +14,7 @@ namespace CustomerReviewVotes.Web
 {
     public class Module : ModuleBase
     {
-        private readonly string _connectionString = ConfigurationHelper.GetConnectionStringValue("VirtoCommerce.newManagedModule.Web") ?? ConfigurationHelper.GetConnectionStringValue("VirtoCommerce");
+        private readonly string _connectionString = ConfigurationHelper.GetConnectionStringValue("VirtoCommerce.CustomerReviewVotes.Web") ?? ConfigurationHelper.GetConnectionStringValue("VirtoCommerce");
         private readonly IUnityContainer _container;
 
         public Module(IUnityContainer container)
@@ -25,11 +25,11 @@ namespace CustomerReviewVotes.Web
         public override void SetupDatabase()
         {
             // Modify database schema with EF migrations
-             using (var db = new CustomerReviewRepository(_connectionString, _container.Resolve<AuditableInterceptor>()))
-             {
-                 var initializer = new SetupDatabaseInitializer<CustomerReviewRepository, Data.Migrations.Configuration>();
+            using (var db = new CustomerReviewRepository(_connectionString, _container.Resolve<AuditableInterceptor>()))
+            {
+                var initializer = new SetupDatabaseInitializer<CustomerReviewRepository, Data.Migrations.Configuration>();
                 initializer.InitializeDatabase(db);
-             }
+            }
         }
 
         public override void Initialize()
@@ -53,8 +53,8 @@ namespace CustomerReviewVotes.Web
             var storeSettingsNames = new[] { "CustomerReviewVotes.CustomerReviewsEnabled" };
             var storeSettings = settingManager.GetModuleSettings("CustomerReviewVotes.Web").Where(x => storeSettingsNames.Contains(x.Name)).ToArray();
             settingManager.RegisterModuleSettings("VirtoCommerce.Store", storeSettings);
-            
- 
+
+
         }
     }
 }
